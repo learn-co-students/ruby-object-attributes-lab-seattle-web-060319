22d4a7fe0efeac735f68d41ea9a312448247a90a
Rohit <rohitpratti@gmail.com>
2019-06-04 16:54:42 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,5 @@
+require "pry"
 class Person
 
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def job
-    @job
-  end
-
-  def job=(new_job)
-    @job = new_job
-  end
-
+  attr_accessor :name, :job
 end
